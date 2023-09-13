f6422012dfb6a601877ea964dc7497af851ee03e
mjh851819 <mjh851819@gmail.com>
2023-09-13 16:49:35 +0900

    [chores] indexController log 추가

--- src/main/java/com/inus/bjtest/controller/IndexController.java
+++ src/main/java/com/inus/bjtest/controller/IndexController.java
@@ -1,16 +1,19 @@
 package com.inus.bjtest.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class IndexController {
 
     @GetMapping("/")
     public ResponseEntity<Void> home() {
+        log.info(" health-check url 요청됨 ");
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
