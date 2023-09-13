a40ea5817b7b10d9e61190e1507b897f49937e4f
mjh851819 <mjh851819@gmail.com>
2023-09-13 15:33:01 +0900

    [feats] IndexController health-check 메서드 추가

--- src/main/java/com/inus/bjtest/controller/IndexController.java
+++ src/main/java/com/inus/bjtest/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.inus.bjtest.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    @GetMapping
-    public String home(){
-        return "ok";
+    @GetMapping("/")
+    public ResponseEntity<Void> home() {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/health-check")
+    public ResponseEntity<Void> checkHealthStatus() {
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
